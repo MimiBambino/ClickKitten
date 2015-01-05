6f8751de80ec1cb0a7c791503aba9edf27d8f49a
Cynthia O'Donnell <mimibambino@gmail.com>
2015-01-05 17:57:13 +0100

    finish version 3

--- js/app.js
+++ js/app.js
@@ -1,21 +1,77 @@
-var value1 = 1;
-var value2 = 1;
+var kittens = {
+  "GrayBeard": {
+    "src": "img/GrayBeard.jpg",
+    "count": 0,
+    "attribution": "By Nicolas Suzor from Brisbane, Australia (Kittens!  Uploaded by Kaldari) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons"
+  },
+  "OrangeBeard": {
+    "src": "img/OrangeBeard.jpg",
+    "count": 0,
+    "attribution": "By Mathias Erhart from Nenzing, Austria (red kitten 01  Uploaded by Kaldari) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons"
+  },
+  "Sasha": {
+    "src": "img/Sasha.jpg",
+    "count": 0,
+    "attribution": "By Patricesf (Own work) [Public domain], via Wikimedia Commons"
+  },
+  "Mr. Buggles": {
+    "src": "img/MrBuggles.jpg",
+    "count": 0,
+    "attribution": "By Steve-h (Flickr: Maggie) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons"
+  },
+  "Tommy": {
+    "src": "img/Tommy.jpg",
+    "count": 0,
+    "attribution": "By Krzysztof P. Jasiutowicz Kpjas (Own work) [CC BY-SA 2.5 (http://creativecommons.org/licenses/by-sa/2.5), GFDL (http://www.gnu.org/copyleft/fdl.html) or CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0/)], via Wikimedia Commons"
+  }
+};
 
-function incrementValue(num) {
-	if (num === 1) {
-		document.getElementById("count1").innerHTML = "Click Count: " + value1;
-  		value1++;
-	} else if (num === 2) {
+function incrementValue(kitten) {
+    kitten['count']++;
 		document.getElementById("count2").innerHTML = "Click Count: " + value2;
   		value2++;
-	}
-
 }
 
-  document.getElementById("pic1").addEventListener("click", function( event ) {
-    incrementValue(1);
-  }, false);
+//for (var i = 0; i < kittens.length; i++) {
 
-  document.getElementById("pic2").addEventListener("click", function( event) {
-  	incrementValue(2);
-  }, false);
+//}
+
+document.getElementById('grayBeard').addEventListener("click", function() {
+  var cat = kittens["GrayBeard"];
+  cat['count']++;
+  document.getElementById('pic').setAttribute('src', cat['src']);
+  document.getElementById('count').innerHTML = "GreyBeard has " + cat['count'] + " clicks";
+  document.getElementById('caption').innerHTML = cat['attribution'];
+});
+
+document.getElementById('orangeBeard').addEventListener("click", function() {
+  var cat = kittens["OrangeBeard"];
+  cat['count']++;
+  document.getElementById('pic').setAttribute('src', cat['src']);
+  document.getElementById('count').innerHTML = "OrangeBeard has " + cat['count'] + " clicks";
+  document.getElementById('caption').innerHTML = cat['attribution'];
+});
+
+document.getElementById('sasha').addEventListener("click", function() {
+  var cat = kittens["Sasha"];
+  cat['count']++;
+  document.getElementById('pic').setAttribute('src', cat['src']);
+  document.getElementById('count').innerHTML = "Sasha has " + cat['count'] + " clicks";
+  document.getElementById('caption').innerHTML = cat['attribution'];
+});
+
+document.getElementById('mrBuggles').addEventListener("click", function() {
+  var cat = kittens["Mr. Buggles"];
+  cat['count']++;
+  document.getElementById('pic').setAttribute('src', cat['src']);
+  document.getElementById('count').innerHTML = "Mr. Buggles has " + cat['count'] + " clicks";
+  document.getElementById('caption').innerHTML = cat['attribution'];
+});
+
+document.getElementById('tommy').addEventListener("click", function() {
+  var cat = kittens["Tommy"];
+  cat['count']++;
+  document.getElementById('pic').setAttribute('src', cat['src']);
+  document.getElementById('count').innerHTML = "Tommy has " + cat['count'] + " clicks";
+  document.getElementById('caption').innerHTML = cat['attribution'];
+});
